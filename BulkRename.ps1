4c726b6c45d811245bad4e61a1493db7199c48cc
Jordan Hall <Jordan_Hall@belk.com>
2019-07-31 15:06:40 -0400

    Working number iterator

--- BulkRename.ps1
+++ BulkRename.ps1
@@ -1,22 +1,23 @@
-$name;
-$files;
-$new_files;
-$directory;
-$placeholder;
-$force = 0;
+$name
+$files
+$new_files
+$directory
+$placeholder
+$num_start_from
+$force = 0
 #detection of option arguments
 function Show-OldAndNewNames {
     for ($i = 0; $i -lt $files.Count; $i++) {
-        "Old Name: " + $files[$i] + " New Name: " + $new_files[$i]
+        "Old Name: " + $files[$i].PadRight(($files | Measure-Object -Maximum -Property Length).Maximum, " ") + " New Name: " + $new_files[$i] #pads out old name then displays
     }
 }
-if ($args.Count -lt 4)
-{
+if ($args.Count -lt 4) {
     "Usage: .\BulkRename.ps1 -n <name> -l <location>"
     "Use the --help argument to get more information"
-    exit;
+    exit
 }
 for ($i = 0; $i -lt $args.Count; $i++) {
+    #argument handling
     if ($args[$i] -like "-n") {
         $name = $args[$i + 1]
     }
@@ -27,27 +28,25 @@ for ($i = 0; $i -lt $args.Count; $i++) {
     elseIf ($args[$i] -like "-q") {
         $placeholder = $args[$i + 1]
     }
-    elseIf ($args[$i] -like "-f") {
+    elseIf ($args[$i] -like "--force") {
         $force = 1
     }
 }
 $new_files = @($name) * $files.Count;
 #detection of everything else
-foreach ($arg in $args) {
-    if ($arg -like "-num") {
-        for($i = 0; $i -lt $new_files.Count; $i++) {
+    for ($i = 0; $i -lt $new_files.Count; $i++) {
+        if ($args.Contains("-num")) {
+            $num_start_from = $args[$args.IndexOf("-num")+1]
             [regex]$pattern = $placeholder
-            $new_files[$i]= $pattern.replace($new_files[$i], $i, 1)
+            $new_files[$i] = $pattern.replace($new_files[$i], $i + $num_start_from, 1)
         }
     }  
-}
+
 Show-OldAndNewNames
-if ($force -eq 0)
-{
+if ($force -eq 0) {
     $response = Read-Host "Is this alright? [yY/nN]"
-    if ($response -ne "y" -or $response -ne "Y")
-    {
-        Write-Host "Exiting..."
+    if ($response -ne "y" -or $response -ne "Y") {
+        "Exiting..."
         exit
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # BulkRename
-Simple Powershell 5.1 script to bulk rename files with argument options
+Simple Powershell 5.1 script to bulk rename files with options and arguments, built to be as customizable and extendable as possible
+#Usage
+.\BulkRename.ps1 -n "<name>" -l "<location>"
+#Options and Arguments
+-n "<name>" : Name to base individual file names off of
+-l "<location>" : Folder location of files
+-q "<placeholder>" : Placeholder to be replaced by one of the replacers
+--force : Bypass new name verification prompt
+#Replacer Usage
+Replacers are used in BulkRename to create unique filenames for each of the files in your folder.
+To use, give BulkRename a single placeholder and a replacer, and the first placeholder found in your name will be replaced using that replacer function
+You can use multiple replacers in one script run, the placeholders in your name will be replaced in the order the replacers are entered into the script run
+
+Example: .\BulkRename.ps1 -n "Base Name * - *" -l "C:\Users\User\File_Location\" -q "*" -num -num
+Name Output: Base Name 0 - 0
+
+Example: .\BulkRename.ps1 -n "Base Name * - *" -l "C:\Users\User\File_Location\" -q "*" -num
+Name Output: Base Name 0 - *
+#Replacers
+-num <starting_number> : Replaces with a iterative number starting at 0 or an optional provided number 
